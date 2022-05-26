bdcf29f21e0e2ab2cfd50ce9b5726ca129330f60
Nix Chaves-Solorzano <acs@umbraco.dk>
2022-05-26 09:05:15 -0400

    full color adjust slider

--- RangeSlider/SliderScript.js
+++ RangeSlider/SliderScript.js
@@ -1,8 +1,45 @@
-﻿var slider = document.getElementById("SliderRange");
-var output = document.getElementById("ValueID");
+﻿var hueSlider = document.getElementById("hueRange");
+var hueOutput = document.getElementById("hueValue");
 
-output.innerHTML = slider.value;
+hueOutput.innerHTML = hueSlider.value;
 
-slider.oninput = function () {
-    output.innerHTML =  this.value;
+hueSlider.oninput = function () {
+    hueOutput.innerHTML =  this.value;
 }
+
+var tintSlider = document.getElementById("tintRange");
+var tintOutput = document.getElementById("tintValue");
+
+tintOutput.innerHTML = tintSlider.value;
+
+tintSlider.oninput = function () {
+    tintOutput.innerHTML = this.value;
+}
+
+var tempSlider = document.getElementById("tempRange");
+var tempOutput = document.getElementById("tempValue");
+
+tempOutput.innerHTML = tempSlider.value;
+
+tempSlider.oninput = function () {
+    tempOutput.innerHTML = this.value;
+}
+
+var satSlider = document.getElementById("satRange");
+var satOutput = document.getElementById("satValue");
+
+satOutput.innerHTML = satSlider.value;
+
+satSlider.oninput = function () {
+    satOutput.innerHTML = this.value;
+}
+
+var vibSlider = document.getElementById("vibRange");
+var vibOutput = document.getElementById("vibValue");
+
+vibOutput.innerHTML = vibSlider.value;
+
+vibSlider.oninput = function () {
+    vibOutput.innerHTML = this.value;
+}
+
