7274c7ddd61e0c13dc38fde13dd447d85c3100d4
Davis Hedgepeth <dnh@umbraco.dk>
2022-06-01 15:56:09 -0400

    Prepping for image manipulation, image now appears on canvas

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-﻿console.log("new script loaded");
+﻿let canvas;
+let context;
 
 var jquery = {
     getLink: function (link) { //function to change image displayed on dashboard given a media link
-        console.log('image changed from script')
-        $('#image').attr('src', link);
+        console.log('image changed from script');
+        $('#sourceImage').attr('src', link);
     }
 };
 
 $(document).ready(function () {
-    $('#image').click(function () {
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext('2d');
+
+    $('#sourceImage').on('load', function () {
+        canvas.width = document.getElementById('sourceImage').width;
+        canvas.height = document.getElementById('sourceImage').height;
+
         
-    })
+        apply();
+    });
 });
+
+function apply() {
+    context.drawImage(document.getElementById('sourceImage'), 0, 0);
+    console.log('testingggg');
+}
